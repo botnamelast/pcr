f6f67e61503ffe17f0e7eee6ae0014e87946ef24
botnamelast <cemmekx096.@gmail.com>
2026-03-14 03:56:42 +0700

    Update patch_runner.py

--- scripts/patch_runner.py
+++ scripts/patch_runner.py
@@ -3,18 +3,17 @@ import re, sys, os
 path = sys.argv[1]
 if not os.path.isfile(path):
     print("RunnerActivity not found!")
-    print("Searching smali dir...")
     import subprocess
     r = subprocess.run(['find', 'pcr_decompiled/smali', '-name', '*.smali'], capture_output=True, text=True)
     print(r.stdout[:2000])
     sys.exit(0)
 
-pkg   = "Lcom/StudioFurukawa/PixelCarRacer"
-line1 = "    new-instance v0, Landroid/content/Intent;"
-line2 = "    const-class v1, {}/ModOverlay;".format(pkg)
-line3 = "    invoke-direct {{v0, p0, v1}}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V"
-line4 = "    invoke-virtual {{p0, v0}}, {}/RunnerActivity;->startService(Landroid/content/Intent;)Landroid/content/ComponentName;".format(pkg)
-inject = "\n".join([line1, line2, line3, line4]) + "\n"
+pkg = "Lcom/StudioFurukawa/PixelCarRacer"
+
+inject  = "    new-instance v0, Landroid/content/Intent;\n"
+inject += "    const-class v1, " + pkg + "/ModOverlay;\n"
+inject += "    invoke-direct {v0, p0, v1}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V\n"
+inject += "    invoke-virtual {p0, v0}, " + pkg + "/RunnerActivity;->startService(Landroid/content/Intent;)Landroid/content/ComponentName;\n"
 
 txt = open(path).read()
 if "ModOverlay" in txt:
